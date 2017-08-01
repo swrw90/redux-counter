638dc225f57e61c1197176f34eea2f26dc264ad6
Seth Watson <seth.watson1@gmail.com>
2017-08-01 15:27:36 -0600

    created actions and reducers folders, created indexjs in actions

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export function up() {
+    return {
+        type: "UP"
+    }
+}
+export function down() {
+    return {
+        type: "DOWN"
+    }
+}
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Counter> </Counter>
+                <CounterContatiner> </CounterContatiner>
              </div>
         )
     }
