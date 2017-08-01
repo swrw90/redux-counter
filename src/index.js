4e191dd059b928c2fe32ff60935f255a0767154b
Seth Watson <seth.watson1@gmail.com>
2017-08-01 15:45:21 -0600

    imported store, installed redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Provider } from "react-redux"; //gives information from store
+import { createStore } from "redux";
+import reducers from "./reducers/";
+
 import CounterContatiner from "./container/counter-container.js";
 
 class App extends React.Component {
