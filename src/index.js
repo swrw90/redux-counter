71231af6b26989425469f49c7daa116f73f68222
Seth Watson <seth.watson1@gmail.com>
2017-08-01 14:27:26 -0600

    removed src files added indexjs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+ class App extends React.Component {
+     render() {
+         return (
+             <div>
+             Test
+             </div>
+         )
+     }
+ }
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.querySelector("#root"));
