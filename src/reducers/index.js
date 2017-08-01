e1fffe1c9445d24c85a7a41156e8bdb61f87a4dc
Seth Watson <seth.watson1@gmail.com>
2017-08-01 15:37:05 -0600

    Modified indexjs reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,19 @@
+let defaultState = {
+    counter: 101
+};
+
+const mainReducer = (state = defaultState, action) => {
+    if (action.type === "UP") {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    } else if (action.type === "DOWN") {
+        return {
+            ...state,
+            counter: state.counter - 1
+        }
+    }
+}
+
+export default mainReducer;
